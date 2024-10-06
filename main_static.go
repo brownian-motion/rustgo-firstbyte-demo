ce4a128416f06404238d2b599313f56e4c286e36
JJ Brown <jj.brown.255@proton.me>
2024-10-06 12:03:03 -0600

    Slight edit

--- main_static.go
+++ main_static.go
@@ -4,11 +4,11 @@ import "fmt"
 import . "github.com/brownian-motion/rustgo-firstbyte-demo/internal"
 
 func main() {
-	print_first_byte("Some letters")
-	print_first_byte("")
+	PrintFirstByte("Some letters")
+	PrintFirstByte("")
 }
 
-func print_first_byte(s string) {
+func PrintFirstByte(s string) {
 	b := FirstByte(s)
 
 	fmt.Printf("The first byte of \"%s\" is '%s' (%x)\n", s, string(b), byte(b))
